1ea83a26a921fd7b2c4f91c04c21c097d4242760
0grazi00 <128594313+0grazi00@users.noreply.github.com>
2024-06-20 07:57:20 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,2 +1,39 @@
-const caixaPrincipal = document.querySelector(".caixa.principal");
-const caixaPerguntas = document.querySelector(".caixa.perguntas");
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+        enunciado: "minha primeira pergunta(digitar aqui)",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: "Segunda pergunta(digitar aqui)",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    }
+    {
+        enunciado: "Terceira pergunta(digitar aqui)",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    }
+];
+
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta () {
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+}
+
+mostraPergunta();
