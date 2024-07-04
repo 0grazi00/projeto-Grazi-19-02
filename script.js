4509217542fc08fc6b9d2006af65a26fff672ca1
0grazi00 <128594313+0grazi00@users.noreply.github.com>
2024-07-04 08:40:56 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -8,22 +8,43 @@ const perguntas = [
     {
         enunciado: "Você já foi seduzido pelo projeto verão? Pela ideia do corpo perfeito, com uma rotina de alimentação e exercícios físicos super bem planejada? ",
         alternativas: [
-            "Sim, a ideia é tentadora!",
-            "Não, já sou feliz com meu corpo!"
+            {
+                texto: "Sim, a ideia é tentadora!",
+                afirmacao: "afirmacao"
+            },
+            {
+                texto:"Não, já sou feliz com meu corpo!",
+                afirmacao: "afirmacao"
+            }
+          
         ]
     },
     {
-        enunciado: "Segunda pergunta(digitar aqui)",
+        enunciado: "Nem sempre é fácil mudar nossos hábitos, não é? Mas os cálculos nos permitem e facilitam esse processo! O que você pensa sobre isso? ",
         alternativas: [
-            "Alternativa 1",
-            "Alternativa 2"
+            {
+                texto: "Os cálculos realmente ajudam!", 
+                afirmacao: "afirmacao"
+            },
+            {
+                texto:"Os cálculos só dificultam o processo!",
+                afirmacao: "afirmacao"
+            }
+           
         ]
     },
     {
-        enunciado: "Terceira pergunta(digitar aqui)",
+        enunciado: "Você sabe como os nutricionistas elaboram as dietas, fichas técnicas ou os cálculos utilizados para fazerem seu planejamento?",
         alternativas: [
-            "Alternativa 1",
-            "Alternativa 2"
+            {
+                texto: "Sim, já tenho conhecimento!",
+                afirmacao: "afirmacao"
+            },
+            {
+                texto: "Não, quero saber mais!",
+                afirmacao: "afirmacao"
+            }
+            
         ]
     }
 ];
@@ -41,7 +62,8 @@ function mostraPergunta () {
 function mostraAlternativas(){
     for(const alternativa of perguntaAtual.alternativas){
         const botaoAlternativas = document.createElement("button")
-        botaoAlternativas.textContent = alternativa;
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(Alternativa)){
         caixaAlternativas.appendChild(botaoAlternativas);
     }
 }
